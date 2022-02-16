33594fa18971538d0feafa2980fc924789b068cd
LiamRICE <71870225+LiamRICE@users.noreply.github.com>
2022-02-16 10:02:25 +0100

    Initial commit

---
+++ dam_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  dam_projectApp.swift
+//  dam-project
+//
+//  Created by m1 on 16/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct dam_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
